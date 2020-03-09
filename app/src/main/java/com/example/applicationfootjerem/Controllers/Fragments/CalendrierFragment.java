2546b2ed1b4782b0eb50098cdf296ceb74444566
jeremi85e <jeremiesemelin@gmail.com>
2020-03-09 12:10:21 +0100

    Ajout boutons journées précédentes et suivantes + Affichage message Toast VolleyError

--- app/src/main/java/com/example/applicationfootjerem/Controllers/Fragments/CalendrierFragment.java
+++ app/src/main/java/com/example/applicationfootjerem/Controllers/Fragments/CalendrierFragment.java
@@ -9,10 +9,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -31,6 +33,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.awt.font.TextAttribute;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -43,11 +46,12 @@ import java.util.Map;
  */
 public class CalendrierFragment extends Fragment{
 
-    private static final String TAG = MainActivity.class.getName();
-    private Button boutonCalendrier;
+    private static final String TAG = "CalendrierFragment";
+    private TextView journeePrecedente;
+    private TextView journeeSuivante;
     private ListView listViewResultats;
     private Spinner spinnerJournees;
-    private Spinner spinnerChampionnats;
+    private int journeeActuelle;
     DateFormat m_ISO8601Local = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
     private RequestQueue mRequestQueue;
@@ -70,34 +74,62 @@ public class CalendrierFragment extends Fragment{
 
         View result = inflater.inflate(R.layout.fragment_calendrier, container, false);
 
-        boutonCalendrier = (Button) result.findViewById(R.id.boutonCalendrier);
         listViewResultats = (ListView) result.findViewById(R.id.calendrierListeView);
         spinnerJournees = (Spinner) result.findViewById(R.id.spinnerJournees);
+        journeePrecedente = (TextView) result.findViewById(R.id.calendrierJourneePrecedente);
+        journeeSuivante = (TextView) result.findViewById(R.id.calendrierJourneeSuivante);
 
-        Integer[] items = new Integer[38];
+        journeeActuelle = getArguments().getInt("currentMatchDay");
+
+        String[] items = new String[38];
         for (int i = 0; i < 38; i++) {
-            items[i] = i+1;
+            items[i] = "Journée " + (i+1);
         }
-        ArrayAdapter<Integer> adapterJournees = new ArrayAdapter<Integer>(getContext(), android.R.layout.simple_spinner_dropdown_item, items);
+        ArrayAdapter<String> adapterJournees = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items);
         spinnerJournees.setAdapter(adapterJournees);
-        spinnerJournees.setSelection(getArguments().getInt("currentMatchDay") - 1);
+        spinnerJournees.setSelection(journeeActuelle - 1);
 
-        boutonCalendrier.setOnClickListener(new View.OnClickListener() {
+
+        getCalendrier(journeeActuelle);
+
+        journeePrecedente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                getCalendrier();
+                journeeActuelle --;
+                getCalendrier(journeeActuelle);
+                spinnerJournees.setSelection(journeeActuelle - 1);
+            }
+        });
+
+        journeeSuivante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                journeeActuelle ++;
+                getCalendrier(journeeActuelle);
+                spinnerJournees.setSelection(journeeActuelle - 1);
+            }
+        });
+
+        spinnerJournees.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                journeeActuelle = position + 1;
+                getCalendrier(journeeActuelle);
+            }
+            public void onNothingSelected(AdapterView<?> parent){
+
             }
         });
 
         return result;
     }
 
-    private void getCalendrier() {
+    private void getCalendrier(int journee) {
         //RequestQueue initialized
         mRequestQueue = Volley.newRequestQueue(getContext());
 
         //String Request initialized
-        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, urlBase + "/competitions/" + getArguments().getString("codeCompetition") + "/matches?matchday=" + spinnerJournees.getSelectedItem().toString(), null, new Response.Listener<JSONObject>() {
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, urlBase + "/competitions/" + getArguments().getString("codeCompetition") + "/matches?matchday=" + journee, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
@@ -131,9 +163,23 @@ public class CalendrierFragment extends Fragment{
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getContext(), "Erreur : " + error.toString(), Toast.LENGTH_LONG).show();
-                Log.i(TAG, "Error :" + error.toString());
-
+                try {
+                    String string = new String(error.networkResponse.data);
+                    JSONObject object = new JSONObject(string);
+                    if (object.has("message")) {
+                        String message = object.get("message").toString();
+                        Toast.makeText(getContext(), "Erreur : " + message, Toast.LENGTH_LONG).show();
+                        Log.i(TAG, "Error :" + message);
+                    }
+                }
+                catch (JSONException e){
+                    Toast.makeText(getContext(), "Erreur : Could not parse response", Toast.LENGTH_LONG).show();
+                    Log.i(TAG, "Erreur : Could not parse response");
+                }
+                catch (NullPointerException e){
+                    Toast.makeText(getContext(), "Erreur : Avez-vous activé Internet ?", Toast.LENGTH_LONG).show();
+                    Log.i(TAG, "Erreur : Avez-vous activé Internet ?");
+                }
             }
         }) {
             @Override
